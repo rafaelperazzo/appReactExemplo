d9d009d000a9b3e341b28ed4efa94dcc1ac71a33
Rafael Mota <rafael.mota@ufca.edu.br>
2023-09-26 09:02:27 -0300

    em desenvolvimento

---
+++ __tests__/testes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {render, screen, fireEvent} from '@testing-library/react-native';
+import '@testing-library/jest-native/extend-expect';
+import App from "../App";
+
+/*
+test('abrir app', () => {
+  render(<App />);
+});
+*/
